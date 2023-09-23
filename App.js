184ea46c8a261173bd40cf2266db0f06c7077410
Kalyan Achyuta <kalyanachyuta@gmail.com>
2023-09-23 22:56:10 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "i am h1 tag")
+//   )
+// );
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "this is h1 tag"),
+//     React.createElement("h2", {}, "this is h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "this is h1 tag"),
+//     React.createElement("h2", {}, "this is h2 tag"),
+//   ]),
+// ]);
+
+const heading = React.createElement("h1", { id: "heading" }, "hello world!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+console.log(root);
+console.log(heading);
